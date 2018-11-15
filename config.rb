4615333024fa5182956c14bf4870ced8ecc31efc
Vincent Beffara <vbeffara@gmail.com>
2018-11-15 10:57:23 +0100

    Proper bibliography in the home page

--- config.rb
+++ config.rb
@@ -1,5 +1,3 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
 activate :syntax
 
@@ -7,47 +5,12 @@ set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
 
 activate :bibtex do |opts|
-    opts.path = 'bib/pub.bib' # path to a bibtex file
+    opts.path   = 'library.bib' # path to a bibtex file
     opts.style  = 'ieee'              # style from citeproc-styles
     opts.format = 'html'              # output format
 end
-
-# Layouts
-# https://middlemanapp.com/basics/layouts/
 
 # Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
